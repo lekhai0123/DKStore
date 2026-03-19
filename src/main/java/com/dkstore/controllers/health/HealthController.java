0f33133fa5cde89491d04bd1821f7e4a6580db2b
Lê Khải <lethanhkhai.career@gmail.com>
2026-03-19 16:24:25 +0700

    health check

---
+++ src/main/java/com/dkstore/controllers/health/HealthController.java
@@ -0,0 +1,12 @@
+package com.dkstore.controllers.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "ok";
+    }
+}
